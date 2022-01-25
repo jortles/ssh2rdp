aba6879416130bf89813ca3e3cd20b708e70e6c8
Anthony Hurtado <amhurtado@protonmail.com>
2022-01-25 13:42:14 -0600

    Added disconnect

--- ssh2rdp/MainWindow.xaml.cs
+++ ssh2rdp/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -16,12 +17,12 @@ namespace ssh2rdp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private secureShell myShell;
+        private SshClient sshclient;
 
         public MainWindow()
         {
             InitializeComponent();
-            myShell = new secureShell();
+            sshDisBtn.Visibility = Visibility.Hidden;
         }
 
         public void Window_MouseMove(object sender, MouseEventArgs e)
@@ -55,7 +56,7 @@ namespace ssh2rdp
         public void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            if (rhost.Text == string.Empty || shost.Text == string.Empty || user.Text == string.Empty || pass.Password == string.Empty)
+            if (rhost.Text == string.Empty || user.Text == string.Empty || pass.Password == string.Empty)
             {
                 MessageBox.Show("Must enter credentials!");
             }
@@ -66,39 +67,32 @@ namespace ssh2rdp
                 var authMethod = new List<AuthenticationMethod>();
                 authMethod.Add(new PasswordAuthenticationMethod(user.Text, pass.Password));
 
-                ConnectionInfo sshConnInfo = new ConnectionInfo(shost.Text, 22, user.Text, authMethod.ToArray());
+                ConnectionInfo sshConnInfo = new ConnectionInfo("", 22, user.Text, authMethod.ToArray());
 
-                SshClient cSSH = new SshClient(sshConnInfo);
+                sshclient = new SshClient(sshConnInfo);
                 var port = new ForwardedPortLocal("127.0.0.1", newPort, rhost.Text, 3389);
 
                 try
                 {
-                    cSSH.Connect();
+                    sshclient.Connect();
                 }
                 catch (Exception)
                 {
-                    cSSH.Disconnect();
+                    sshclient.Disconnect();
                 }
                 finally
                 {
-                    if (cSSH.IsConnected)
+                    if (sshclient.IsConnected)
                     {
-                        rhost.IsReadOnly = true;
-                        user.IsReadOnly = true;
-
-
-                        cSSH.AddForwardedPort(port);
+                        sshDisBtn.Visibility = Visibility.Visible;
+                        sshclient.AddForwardedPort(port);
 
                         port.Start();
 
-                        sshBtn.Background = Brushes.LimeGreen;
-                        sshBtn.Content = "Connected!";
-                        sshBtn.IsEnabled = false;
-
                         List<secureShell> myShell = new List<secureShell>();
-                        if (cSSH.IsConnected)
+                        if (sshclient.IsConnected)
                         {
-                            myShell.Add(new secureShell() { host = cSSH.ConnectionInfo.Host, username = cSSH.ConnectionInfo.Username, lPort = (int)newPort });
+                            myShell.Add(new secureShell() { host = sshclient.ConnectionInfo.Host, remote = rhost.Text, username = sshclient.ConnectionInfo.Username, lPort = (int)newPort });
                         }
 
                         listViewShell.ItemsSource = myShell;
@@ -122,6 +116,10 @@ namespace ssh2rdp
                         process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
                         process.StartInfo.Arguments = string.Format("{0} /f /v {1}", "C:\\Users\\" + user.Text + "\\Documents\\Default.rdp", "127.0.0.1:" + lp);
                         process.Start();
+
+                        rhost.Clear();
+                        user.Clear();
+                        pass.Clear();
                     }
                     else
                     {
@@ -130,11 +128,27 @@ namespace ssh2rdp
                 }
             }
         }
+
+        public void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (sshclient.IsConnected)
+            {
+                sshclient.Disconnect();
+                sshclient.Dispose();
+                listViewShell.ClearValue(ListView.ItemsSourceProperty);
+                sshDisBtn.Visibility = Visibility.Collapsed;
+            }
+            else if (!sshclient.IsConnected)
+            {
+                MessageBox.Show(sshclient.ConnectionInfo.ToString());
+            }
+        }
     }
 
     public class secureShell
     {
         public string host { get; set; }
+        public string remote { get; set; }
         public string username { get; set; }
         public int lPort { get; set; }
     }
